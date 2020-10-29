f37d3b842c6500d6967043ce0f9a14643aee99ad
mpinol <michaelp@unity3d.com>
2020-10-29 16:57:21 -0700

    Removing extra ROS packages.

--- tutorials/pick_and_place/ROS/tcp_endpoint/src/tcp_endpoint/TCPEndpointExceptions.py
+++
@@ -1,8 +0,0 @@
-class Error(Exception):
-    """Base class for other exceptions"""
-    pass
-
-
-class TopicOrServiceNameDoesNotExistError(Error):
-    """The topic or service name passed does not exist in the source destination dictionary."""
-    pass
