fb9c854f220d8af83406481727b9b445a2b371cc
LaurieCheers <73140792+LaurieCheers-unity@users.noreply.github.com>
2020-11-17 09:48:11 -0800

    Updates to the pick and place tutorial

--- tutorials/pick_and_place/ROS/src/niryo_moveit/scripts/server_endpoint.py
+++ tutorials/pick_and_place/ROS/src/niryo_moveit/scripts/server_endpoint.py
@@ -2,6 +2,7 @@
 
 import rospy
 
+from tcp_endpoint.UnityTCPSender import UnityTCPSender
 from tcp_endpoint.RosTCPServer import TCPServer
 from tcp_endpoint.RosPublisher import RosPublisher
 from tcp_endpoint.RosSubscriber import RosSubscriber
@@ -14,25 +15,27 @@ def main():
     # Get variables set in rosparam used for
     # server/node communication
     ros_tcp_ip = rospy.get_param("/ROS_IP")
-    ros_tcp_port = rospy.get_param("/ROS_TCP_PORT")
+    ros_tcp_port = rospy.get_param("/ROS_TCP_PORT", 10000)
 
     ros_node_name = rospy.get_param("/TCP_NODE_NAME", 'TCPServer')
 
-    unity_machine_ip = rospy.get_param("/UNITY_IP")
-    unity_machine_port = rospy.get_param("/UNITY_SERVER_PORT")
+    unity_machine_ip = rospy.get_param("/UNITY_IP", '')
+    unity_machine_port = rospy.get_param("/UNITY_SERVER_PORT", 5005)
 
     rospy.init_node(ros_node_name, anonymous=True)
     rate = rospy.Rate(10)  # 10hz
 
+    tcp_sender = UnityTCPSender(unity_machine_ip, unity_machine_port)
+
     # Create ROS communication objects dictionary for routing messages
     source_destination_dict = {
         'SourceDestination_input': RosPublisher('SourceDestination', NiryoMoveitJoints, queue_size=10),
-        'NiryoTrajectory': RosSubscriber('NiryoTrajectory', NiryoTrajectory, unity_machine_ip, unity_machine_port),
+        'NiryoTrajectory': RosSubscriber('NiryoTrajectory', NiryoTrajectory, tcp_sender),
         'niryo_moveit': RosService('niryo_moveit', MoverService)
     }
 
     # Start the Server Endpoint
-    tcp_server = TCPServer(ros_tcp_ip, ros_tcp_port, ros_node_name, source_destination_dict)
+    tcp_server = TCPServer(ros_tcp_ip, ros_tcp_port, tcp_sender, ros_node_name, source_destination_dict)
     tcp_server.start()
     rospy.spin()
 
