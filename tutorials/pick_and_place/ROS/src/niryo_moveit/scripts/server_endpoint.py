555a1210161722ed424b9ea2ab389f9f647254aa
LaurieCheers <73140792+LaurieCheers-unity@users.noreply.github.com>
2020-11-18 10:47:20 -0800

    Moved logic into TCPServer to simplify server_endpoints

--- tutorials/pick_and_place/ROS/src/niryo_moveit/scripts/server_endpoint.py
+++ tutorials/pick_and_place/ROS/src/niryo_moveit/scripts/server_endpoint.py
@@ -2,7 +2,6 @@
 
 import rospy
 
-from tcp_endpoint.UnityTCPSender import UnityTCPSender
 from tcp_endpoint.RosTCPServer import TCPServer
 from tcp_endpoint.RosPublisher import RosPublisher
 from tcp_endpoint.RosSubscriber import RosSubscriber
@@ -12,30 +11,18 @@ from niryo_moveit.msg import NiryoMoveitJoints, NiryoTrajectory
 from niryo_moveit.srv import MoverService
 
 def main():
-    # Get variables set in rosparam used for
-    # server/node communication
-    ros_tcp_ip = rospy.get_param("/ROS_IP")
-    ros_tcp_port = rospy.get_param("/ROS_TCP_PORT", 10000)
-
     ros_node_name = rospy.get_param("/TCP_NODE_NAME", 'TCPServer')
-
-    unity_machine_ip = rospy.get_param("/UNITY_IP", '')
-    unity_machine_port = rospy.get_param("/UNITY_SERVER_PORT", 5005)
-
-    rospy.init_node(ros_node_name, anonymous=True)
-    rate = rospy.Rate(10)  # 10hz
-
-    tcp_sender = UnityTCPSender(unity_machine_ip, unity_machine_port)
+    tcp_server = TCPServer(ros_node_name)
 
     # Create ROS communication objects dictionary for routing messages
-    source_destination_dict = {
+    tcp_server.source_destination_dict = {
         'SourceDestination_input': RosPublisher('SourceDestination', NiryoMoveitJoints, queue_size=10),
-        'NiryoTrajectory': RosSubscriber('NiryoTrajectory', NiryoTrajectory, tcp_sender),
+        'NiryoTrajectory': RosSubscriber('NiryoTrajectory', NiryoTrajectory, tcp_server),
         'niryo_moveit': RosService('niryo_moveit', MoverService)
     }
 
     # Start the Server Endpoint
-    tcp_server = TCPServer(ros_tcp_ip, ros_tcp_port, tcp_sender, ros_node_name, source_destination_dict)
+    rospy.init_node(ros_node_name, anonymous=True)
     tcp_server.start()
     rospy.spin()
 
--- tutorials/ros_packages/robotics_demo/scripts/server_endpoint.py
+++ tutorials/ros_packages/robotics_demo/scripts/server_endpoint.py
@@ -3,7 +3,6 @@
 import rospy
 
 from tcp_endpoint.RosTCPServer import TCPServer
-from tcp_endpoint.UnityTCPSender import UnityTCPSender
 from tcp_endpoint.RosPublisher import RosPublisher
 from tcp_endpoint.RosSubscriber import RosSubscriber
 from tcp_endpoint.RosService import RosService
@@ -12,30 +11,18 @@ from robotics_demo.msg import PosRot, UnityColor
 from robotics_demo.srv import PositionService
 
 def main():
-    ros_tcp_ip = rospy.get_param("/ROS_IP")
-    ros_tcp_port = rospy.get_param("/ROS_TCP_PORT", 10000)
-
     ros_node_name = rospy.get_param("/TCP_NODE_NAME", 'TCPServer')
     buffer_size = rospy.get_param("/TCP_BUFFER_SIZE", 1024)
     connections = rospy.get_param("/TCP_CONNECTIONS", 10)
+    tcp_server = TCPServer(ros_node_name, buffer_size, connections)
 
-    #Forcing the Unity IP is optional
-    unity_machine_ip = rospy.get_param("/UNITY_IP", '')
-    unity_machine_port = rospy.get_param("/UNITY_SERVER_PORT", 5005)
-
-    rospy.init_node(ros_node_name, anonymous=True)
-    rate = rospy.Rate(10)  # 10hz
-
-    tcp_sender = UnityTCPSender(unity_machine_ip, unity_machine_port)
-
-    source_destination_dict = {
+    tcp_server.source_destination_dict = {
         'pos_srv': RosService('position_service', PositionService),
         'pos_rot': RosPublisher('pos_rot', PosRot, queue_size=10),
-        'color': RosSubscriber('color', UnityColor, tcp_sender)
+        'color': RosSubscriber('color', UnityColor, tcp_server)
     }
 
-    tcp_server = TCPServer(ros_tcp_ip, ros_tcp_port, tcp_sender,
-                           ros_node_name, source_destination_dict, buffer_size, connections)
+    rospy.init_node(ros_node_name, anonymous=True)
     tcp_server.start()
     rospy.spin()
 
