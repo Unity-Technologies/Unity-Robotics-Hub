33747631e2aaa6ae64a03e73702a59fa9bacb917
LaurieCheers <73140792+LaurieCheers-unity@users.noreply.github.com>
2021-01-22 09:48:35 -0800

    Final commits for v0.1.2

--- tutorials/pick_and_place/ROS/src/niryo_moveit/scripts/server_endpoint.py
+++ tutorials/pick_and_place/ROS/src/niryo_moveit/scripts/server_endpoint.py
@@ -3,7 +3,6 @@
 import rospy
 
 from ros_tcp_endpoint import TcpServer, RosPublisher, RosSubscriber, RosService
-
 from niryo_moveit.msg import NiryoMoveitJoints, NiryoTrajectory
 from niryo_moveit.srv import MoverService
 
@@ -11,17 +10,16 @@ from niryo_moveit.srv import MoverService
 def main():
     ros_node_name = rospy.get_param("/TCP_NODE_NAME", 'TCPServer')
     tcp_server = TcpServer(ros_node_name)
+    tcp_server.source_destination_dict = 
+    rospy.init_node(ros_node_name, anonymous=True)
 
-    # Create ROS communication objects dictionary for routing messages
-    tcp_server.source_destination_dict = {
+    # Start the Server Endpoint with a ROS communication objects dictionary for routing messages
+    tcp_server.start({
         'SourceDestination_input': RosPublisher('SourceDestination', NiryoMoveitJoints, queue_size=10),
         'NiryoTrajectory': RosSubscriber('NiryoTrajectory', NiryoTrajectory, tcp_server),
         'niryo_moveit': RosService('niryo_moveit', MoverService)
-    }
-
-    # Start the Server Endpoint
-    rospy.init_node(ros_node_name, anonymous=True)
-    tcp_server.start()
+    })
+    
     rospy.spin()
 
 
