a36bfa05f6b43bafd57365457c940d4110248646
LaurieCheers <73140792+LaurieCheers-unity@users.noreply.github.com>
2021-01-22 09:51:55 -0800

    Oops

--- tutorials/pick_and_place/ROS/src/niryo_moveit/scripts/server_endpoint.py
+++ tutorials/pick_and_place/ROS/src/niryo_moveit/scripts/server_endpoint.py
@@ -10,7 +10,6 @@ from niryo_moveit.srv import MoverService
 def main():
     ros_node_name = rospy.get_param("/TCP_NODE_NAME", 'TCPServer')
     tcp_server = TcpServer(ros_node_name)
-    tcp_server.source_destination_dict = 
     rospy.init_node(ros_node_name, anonymous=True)
 
     # Start the Server Endpoint with a ROS communication objects dictionary for routing messages
