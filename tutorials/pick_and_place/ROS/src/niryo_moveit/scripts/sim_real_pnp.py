f2dbd33473695db7e7fea894bfd57dba6e5da0c6
mpinol <mpinol@users.noreply.github.com>
2021-02-08 08:54:48 -0800

    Mpinol/simrealrebased (#107)
    * Adding sim and real pnp files.

---
+++ tutorials/pick_and_place/ROS/src/niryo_moveit/scripts/sim_real_pnp.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import rospy
+
+import time
+import sys
+import copy
+import math
+import moveit_commander
+import actionlib
+
+import moveit_msgs.msg
+
+from niryo_one_msgs.msg import RobotMoveAction, ToolCommand, TrajectoryPlan
+from niryo_one_msgs.msg import RobotMoveGoal
+
+from moveit_msgs.msg import Constraints, JointConstraint, PositionConstraint, OrientationConstraint, BoundingVolume
+from sensor_msgs.msg import JointState
+from moveit_msgs.msg import RobotState
+import geometry_msgs.msg
+from geometry_msgs.msg import Quaternion, Pose
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+from niryo_moveit.srv import MoverServiceRequest
+
+joint_names = ['joint_1', 'joint_2', 'joint_3', 'joint_4', 'joint_5', 'joint_6']
+
+# Gripper parameters
+OPEN_COMMAND = 1
+CLOSE_COMMAND = 2
+GRIPPER_SPEED = 100
+
+# IDs used to determine which values in a RobotMoveGoal message to execute
+TOOL_ID = 11
+TOOL_COMMAND_ID = 6
+TRAJECTORY_COMMAND_ID = 7
+
+PICK_PLACE_HEIGHT_OFFSET = 0.065 
+
+# Between Melodic and Noetic, the return type of plan() changed. moveit_commander has no __version__ variable, so checking the python version as a proxy
+if sys.version_info >= (3, 0):
+    def planCompat(plan):
+        return plan[1]
+else:
+    def planCompat(plan):
+        return plan
+
+"""
+    Given the start angles of the robot, plan a trajectory that ends at the destination pose.
+"""
+def plan_trajectory(move_group, destination_pose, start_joint_angles): 
+    current_joint_state = JointState()
+    current_joint_state.name = joint_names
+    current_joint_state.position = start_joint_angles
+
+    moveit_robot_state = RobotState()
+    moveit_robot_state.joint_state = current_joint_state
+    move_group.set_start_state(moveit_robot_state)
+
+    move_group.set_pose_target(destination_pose)
+
+    if not move_group.plan():
+        exception_str = """
+            Trajectory could not be planned for a destination of {}\n with starting joint angles {}.
+            Please make sure target and destination are reachable by the robot.
+        """.format(destination_pose, start_joint_angles)
+        raise Exception(exception_str)
+
+    trajectory_plan = TrajectoryPlan()
+    trajectory_plan.trajectory = planCompat(move_group.plan())
+
+    return trajectory_plan
+
+
+def send_trajectory_goal(client, trajectory):
+
+    # Build the goal
+    goal = RobotMoveGoal()
+    goal.cmd.Trajectory = trajectory
+    goal.cmd.cmd_type = TRAJECTORY_COMMAND_ID
+
+    client.send_goal(goal)
+    client.wait_for_result()
+
+    return
+
+def send_tool_goal(client, gripper_command):
+    tool_command = ToolCommand()
+    tool_command.tool_id = TOOL_ID
+    tool_command.cmd_type = gripper_command
+    tool_command.gripper_open_speed = GRIPPER_SPEED
+    tool_command.gripper_close_speed = GRIPPER_SPEED
+
+    goal = RobotMoveGoal()
+    goal.cmd.tool_cmd = tool_command
+    goal.cmd.cmd_type = TOOL_COMMAND_ID
+
+    client.send_goal(goal)
+    client.wait_for_result()
+
+    return
+
+
+"""
+    Creates a pick and place plan using the four states below.
+    
+    1. Pre Grasp - position gripper directly above target object
+    2. Grasp - lower gripper so that fingers are on either side of object
+    3. Pick Up - raise gripper back to the pre grasp position
+    4. Place - move gripper to desired placement position
+
+    Gripper behaviour is handled outside of this trajectory planning.
+        - Gripper close occurs after 'grasp' position has been achieved
+        - Gripper open occurs after 'place' position has been achieved
+
+    https://github.com/ros-planning/moveit/blob/master/moveit_commander/src/moveit_commander/move_group.py
+"""
+def plan_pick_and_place(req):
+    print("Starting planning....")
+    client = actionlib.SimpleActionClient('niryo_one/commander/robot_action', RobotMoveAction)
+    client.wait_for_server()
+
+    group_name = "arm"
+    move_group = moveit_commander.MoveGroupCommander(group_name)
+
+    current_robot_joint_configuration = [
+        math.radians(req.joints_input.joint_00),
+        math.radians(req.joints_input.joint_01),
+        math.radians(req.joints_input.joint_02),
+        math.radians(req.joints_input.joint_03),
+        math.radians(req.joints_input.joint_04),
+        math.radians(req.joints_input.joint_05),
+    ]
+
+    # Pre grasp - position gripper directly above target object
+    pre_grasp_pose = plan_trajectory(move_group, req.pick_pose, current_robot_joint_configuration)
+
+    previous_ending_joint_angles = pre_grasp_pose.trajectory.joint_trajectory.points[-1].positions
+    send_trajectory_goal(client, pre_grasp_pose)
+
+    # Grasp - lower gripper so that fingers are on either side of object
+    pick_pose = copy.deepcopy(req.pick_pose)
+    pick_pose.position.z -= PICK_PLACE_HEIGHT_OFFSET  # Static value coming from Unity, TODO: pass along with request
+    grasp_pose = plan_trajectory(move_group, pick_pose, previous_ending_joint_angles)
+
+    previous_ending_joint_angles = grasp_pose.trajectory.joint_trajectory.points[-1].positions
+    send_trajectory_goal(client, grasp_pose)
+    send_tool_goal(client, CLOSE_COMMAND)
+
+    # Pick Up - raise gripper back to the pre grasp position
+    pick_up_pose = plan_trajectory(move_group, req.pick_pose, previous_ending_joint_angles)
+    previous_ending_joint_angles = pick_up_pose.trajectory.joint_trajectory.points[-1].positions
+    send_trajectory_goal(client, pick_up_pose)
+
+    # Place - move gripper to desired placement position
+    pre_place_pose = plan_trajectory(move_group, req.place_pose, previous_ending_joint_angles)
+    previous_ending_joint_angles = pre_place_pose.trajectory.joint_trajectory.points[-1].positions
+    send_trajectory_goal(client, pre_place_pose)
+
+    # Gently Place object
+    place_pose = copy.deepcopy(req.place_pose)
+    place_pose.position.z -= PICK_PLACE_HEIGHT_OFFSET  # Static value coming from Unity, TODO: pass along with request
+    place_traj = plan_trajectory(move_group, place_pose, previous_ending_joint_angles)
+    previous_ending_joint_angles = place_traj.trajectory.joint_trajectory.points[-1].positions
+    send_trajectory_goal(client, place_traj)
+    send_tool_goal(client, OPEN_COMMAND)
+
+    # Reset robot at pre place pose pose
+    post_place_traj = plan_trajectory(move_group, req.place_pose, previous_ending_joint_angles)
+    send_trajectory_goal(client, post_place_traj)
+
+    print("Finished executing action.")
+
+def listener():
+    rospy.init_node('sim_real_pnp', anonymous=True)
+
+    rospy.Subscriber("sim_real_pnp", MoverServiceRequest, plan_pick_and_place)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
---
+++ tutorials/pick_and_place/Scripts/RealSimPickAndPlace.cs
@@ -0,0 +1,206 @@
+using System.Collections;
+using System.Linq;
+using ROSGeometry;
+using RosMessageTypes.Moveit;
+using RosMessageTypes.NiryoMoveit;
+using RosMessageTypes.NiryoOne;
+using UnityEngine;
+using Pose = RosMessageTypes.Geometry.Pose;
+using RosQuaternion = RosMessageTypes.Geometry.Quaternion;
+
+public class RealSimPickAndPlace : MonoBehaviour
+{
+    private const int OPEN_GRIPPER = 1;
+    private const int CLOSE_GRIPPER = 2;
+    private const int TOOL_COMMAND_EXECUTION = 6;
+    private const int TRAJECTORY_COMMAND_EXECUTION = 7;
+    
+    private ROSConnection ros;
+    private const int NUM_ROBOT_JOINTS = 6;
+    
+    // Hardcoded variables 
+    private const float JOINT_ASSIGNMENT_WAIT = 0.038f;
+    private readonly Vector3 PICK_POSE_OFFSET = Vector3.up * 0.15f;
+    
+    // Assures that the gripper is always positioned above the target cube before grasping.
+    private readonly Quaternion pickOrientation = Quaternion.Euler(90, 90, 0);
+
+    // Variables required for ROS communication
+    public string rosJointPublishTopicName = "sim_real_pnp";
+    public string rosRobotCommandsTopicName = "niryo_one/commander/robot_action/goal";
+
+    public GameObject niryoOne;
+    public GameObject target;
+    public GameObject targetPlacement;
+
+    // Articulation Bodies
+    private ArticulationBody[] jointArticulationBodies;
+    private ArticulationBody leftGripperJoint;
+    private ArticulationBody rightGripperJoint;
+
+    private Transform leftGripperGameObject;
+    private Transform rightGripperGameObject;
+
+    /// <summary>
+    ///     Close the gripper
+    /// </summary>
+    private void CloseGripper()
+    {
+        var leftDrive = leftGripperJoint.xDrive;
+        var rightDrive = rightGripperJoint.xDrive;
+
+        leftDrive.target = 0.01f;
+        rightDrive.target = -0.01f;
+
+        leftGripperJoint.xDrive = leftDrive;
+        rightGripperJoint.xDrive = rightDrive;
+    }
+
+    /// <summary>
+    ///     Open the gripper
+    /// </summary>
+    private void OpenGripper()
+    {
+        var leftDrive = leftGripperJoint.xDrive;
+        var rightDrive = rightGripperJoint.xDrive;
+
+        leftDrive.target = -0.01f;
+        rightDrive.target = 0.01f;
+
+        leftGripperJoint.xDrive = leftDrive;
+        rightGripperJoint.xDrive = rightDrive;
+    }
+
+
+    /// <summary>
+    ///     Publish the robot's current joint configuration, the target object's
+    ///     position and rotation, and the target placement for the target object's
+    ///     position and rotation.
+    ///
+    ///     Includes conversion from Unity coordinates to ROS coordinates, Forward Left Up
+    /// </summary>
+    public void PublishJoints()
+    {
+        MoverServiceRequest request = new MoverServiceRequest
+        {
+            joints_input =
+            {
+                joint_00 = jointArticulationBodies[0].xDrive.target,
+                joint_01 = jointArticulationBodies[1].xDrive.target,
+                joint_02 = jointArticulationBodies[2].xDrive.target,
+                joint_03 = jointArticulationBodies[3].xDrive.target,
+                joint_04 = jointArticulationBodies[4].xDrive.target,
+                joint_05 = jointArticulationBodies[5].xDrive.target
+            },
+            pick_pose = new Pose
+            {
+                position = (target.transform.position + PICK_POSE_OFFSET).To<FLU>(),
+                // The hardcoded x/z angles assure that the gripper is always positioned above the target cube before grasping.
+                orientation = Quaternion.Euler(90, target.transform.eulerAngles.y, 0).To<FLU>()
+            },
+            place_pose = new Pose
+            {
+                position = (targetPlacement.transform.position + PICK_POSE_OFFSET).To<FLU>(),
+                orientation = pickOrientation.To<FLU>()
+            }
+        };
+        
+        ros.Send(rosJointPublishTopicName, request);
+    }
+    
+    /// <summary>
+    ///     Find all robot joints in Awake() and add them to the jointArticulationBodies array.
+    ///     Find left and right finger joints and assign them to their respective articulation body objects.
+    /// </summary>
+    void Awake()
+    {
+        jointArticulationBodies = new ArticulationBody[NUM_ROBOT_JOINTS];
+        string shoulder_link = "world/base_link/shoulder_link";
+        jointArticulationBodies[0] = niryoOne.transform.Find(shoulder_link).GetComponent<ArticulationBody>();
+
+        string armLink = shoulder_link + "/arm_link";
+        jointArticulationBodies[1] = niryoOne.transform.Find(armLink).GetComponent<ArticulationBody>();
+        
+        string elbowLink = armLink + "/elbow_link";
+        jointArticulationBodies[2] = niryoOne.transform.Find(elbowLink).GetComponent<ArticulationBody>();
+        
+        string forearmLink = elbowLink + "/forearm_link";
+        jointArticulationBodies[3] = niryoOne.transform.Find(forearmLink).GetComponent<ArticulationBody>();
+        
+        string wristLink = forearmLink + "/wrist_link";
+        jointArticulationBodies[4] = niryoOne.transform.Find(wristLink).GetComponent<ArticulationBody>();
+        
+        string handLink = wristLink + "/hand_link";
+        jointArticulationBodies[5] = niryoOne.transform.Find(handLink).GetComponent<ArticulationBody>();
+
+        // Find left and right fingers
+        string rightGripper = handLink + "/tool_link/gripper_base/servo_head/control_rod_right/right_gripper";
+        string leftGripper = handLink + "/tool_link/gripper_base/servo_head/control_rod_left/left_gripper";
+
+        leftGripperGameObject = niryoOne.transform.Find(leftGripper);
+        rightGripperGameObject = niryoOne.transform.Find(rightGripper);
+
+        rightGripperJoint = rightGripperGameObject.GetComponent<ArticulationBody>();
+        leftGripperJoint = leftGripperGameObject.GetComponent<ArticulationBody>();
+    }
+    
+    void Start()
+    {
+        ros.Subscribe<RobotMoveActionGoal>(rosRobotCommandsTopicName, ExecuteRobotCommands);
+    }
+
+    /// <summary>
+    ///   Execute robot commands receved from ROS Subscriber.
+    ///   Gripper commands will be executed immeditately wihle trajectories will be 
+    ///   executed in a coroutine.
+    /// </summary>
+    /// <param name="robotAction"> RobotMoveActionGoal of trajectory or gripper commands</param>
+    void ExecuteRobotCommands(RobotMoveActionGoal robotAction)
+    {
+        if (robotAction.goal.cmd.cmd_type == TRAJECTORY_COMMAND_EXECUTION)
+        {
+            StartCoroutine(ExecuteTrajectories(robotAction.goal.cmd.Trajectory.trajectory));
+        }
+        else if (robotAction.goal.cmd.cmd_type == TOOL_COMMAND_EXECUTION)
+        {
+            if (robotAction.goal.cmd.tool_cmd.cmd_type == OPEN_GRIPPER)
+            {
+                Debug.Log("Open Tool Command");
+                OpenGripper();
+            }
+            else if (robotAction.goal.cmd.tool_cmd.cmd_type == CLOSE_GRIPPER)
+            {
+                Debug.Log("Close Tool Command");
+                CloseGripper();
+            }
+        }
+    }
+
+    /// <summary>
+    ///     Execute trajectories from RobotMoveActionGoal topic.
+    /// 
+    ///     Execution will iterate through every robot pose in the trajectory pose 
+    ///     array while updating the joint values on the simulated robot.
+    /// 
+    /// </summary>
+    /// <param name="trajectories"> The array of poses for the robot to execute</param>
+    private IEnumerator ExecuteTrajectories(RobotTrajectory trajectories)
+    {
+        // For every robot pose in trajectory plan
+        foreach (var point in trajectories.joint_trajectory.points)
+        {
+            var jointPositions = point.positions;
+            float[] result = jointPositions.Select(r=> (float)r * Mathf.Rad2Deg).ToArray();
+            
+            // Set the joint values for every joint
+            for (int joint = 0; joint < jointArticulationBodies.Length; joint++)
+            {
+                var joint1XDrive  = jointArticulationBodies[joint].xDrive;
+                joint1XDrive.target = result[joint];
+                jointArticulationBodies[joint].xDrive = joint1XDrive;
+            }
+            // Wait for robot to achieve pose for all joint assignments
+            yield return new WaitForSeconds(JOINT_ASSIGNMENT_WAIT);
+        }
+    }
+}
