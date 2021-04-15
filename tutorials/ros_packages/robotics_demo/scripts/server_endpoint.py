42bf426e67da2ad6bef09e5ed6c39333d6f58c4a
Hamid Younesy <hyounesy@users.noreply.github.com>
2021-04-14 18:35:30 -0700

    Unity service example (#199)
    * added example of implementing a UnityService
    
    * adding tutorial for unity service
    
    * added a couple comments to the example code
    
    * typos and code standard fixes
    
    * consistency: changing the service name for position_service to pos_srv
    
    * updating the tutorial to match the corresponding script

--- tutorials/ros_packages/robotics_demo/scripts/server_endpoint.py
+++ tutorials/ros_packages/robotics_demo/scripts/server_endpoint.py
@@ -2,25 +2,24 @@
 
 import rospy
 
-from ros_tcp_endpoint import TcpServer, RosPublisher, RosSubscriber, RosService
-
+from ros_tcp_endpoint import TcpServer, RosPublisher, RosSubscriber, RosService, UnityService
 from robotics_demo.msg import PosRot, UnityColor
-from robotics_demo.srv import PositionService
+from robotics_demo.srv import PositionService, ObjectPoseService
 
 def main():
     ros_node_name = rospy.get_param("/TCP_NODE_NAME", 'TCPServer')
     buffer_size = rospy.get_param("/TCP_BUFFER_SIZE", 1024)
     connections = rospy.get_param("/TCP_CONNECTIONS", 10)
     tcp_server = TcpServer(ros_node_name, buffer_size, connections)
-
-    tcp_server.source_destination_dict = {
-        'pos_srv': RosService('position_service', PositionService),
-        'pos_rot': RosPublisher('pos_rot', PosRot, queue_size=10),
-        'color': RosSubscriber('color', UnityColor, tcp_server)
-    }
-
     rospy.init_node(ros_node_name, anonymous=True)
-    tcp_server.start()
+    
+    tcp_server.start({
+        'pos_rot': RosPublisher('pos_rot', PosRot, queue_size=10),
+        'color': RosSubscriber('color', UnityColor, tcp_server),
+        'pos_srv': RosService('pos_srv', PositionService),
+        'obj_pose_srv': UnityService('obj_pose_srv', ObjectPoseService, tcp_server),
+    })
+    
     rospy.spin()
 
 
